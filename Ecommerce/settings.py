f708c7ce65a9bdfb27334e01cce54381a06299ed
namlk173 <lekhacnam1703@gmail.com>
2022-07-26 23:26:38 +0700

    commit for deploy

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y^y%(*@599gamtxr5!yfgm&m)8m*^sn5@kaf#@-$ga$-ho$kmr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'myapp-lkn.herokuapp.com']
 
 
 # Application definition
