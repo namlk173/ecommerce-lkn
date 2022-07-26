54a279fcddbb819cc896f5c748fbd2f6fbc0ce5a
namlk173 <lekhacnam1703@gmail.com>
2022-07-26 23:49:16 +0700

    commit for deploy

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -133,7 +133,6 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
-STATIC_ROOT = BASE_DIR / 'static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
